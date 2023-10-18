7fb38aad7fea20771d92cf01e77d58e2201a9b55
Valera <leo.plv@bk.ru>
2023-10-18 11:03:17 +0500

    домашнее задание 6

--- sem5.py
+++ sem5.py
@@ -1,22 +1,5 @@
-def binary_search(arr, num):
-    left = 0
-    right = len(arr) - 1
+sum_list([], 0).
 
-    while left <= right:
-        mid = (left + right) // 2
-
-        if arr[mid] == num:
-            return mid
-        elif arr[mid] < num:
-            left = mid + 1
-        else:
-            right = mid - 1
-
-    return -1
-
- 
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-num = 4
-
-index = binary_search(arr, num)
-print("Индекс элемента:", index)
+sum_list([Head|Tail], Sum) :-
+sum_list(Tail, RestSum),
+Sum is Head + RestSum.
